be8a0abb80ef0134ce4cde77ecc971f9c413111d
alan-leal <78099059+alan-leal@users.noreply.github.com>
2021-06-04 14:29:30 -0400

    Update simpleCalculator.java
    - added reset feature
    - added error handling via string prompt

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -1,5 +1,3 @@
-package Summer2021;
-
 import java.util.Scanner;
 
 public class simpleCalculator {
@@ -7,8 +5,9 @@ public class simpleCalculator {
         // write your code here
         System.out.println("It's Going To Be A Good Day Alan ~ 6/3/2021");
 
-
-            System.out.print("Enter a number: ");
+        boolean newCalculation = true;
+        while (newCalculation) {
+            System.out.print("Enter your first number: ");
             Scanner input = new Scanner(System.in);
             int number1 = input.nextInt();
             System.out.print("Enter your second number: ");
@@ -36,45 +35,59 @@ public class simpleCalculator {
             } else if (userResponse == '/') {
                 System.out.print(number1 + " divided by " + number2 + " equals -> ");
                 System.out.println((number1) / (number2));
-                System.out.println("");
+                System.out.println();
                 result = number1 / number2;
             }
 
-        boolean endlessMath = true;
-        while (endlessMath) {
-            System.out.println("Would you like to configure another digit?");
-            String response = input.next();
-
-            if (!response.equalsIgnoreCase("YES")) {
-                endlessMath = false;
-                System.out.println("                Understood" + "\n" + " <3 visit xoxocity again in your near future! <3");
-            } else {
-                System.out.print("Enter your next digit: ");
-                int userInput = input.nextInt();
+            boolean endlessMath = true;
+            while (endlessMath) {
+                System.out.println("Would you like to configure another digit or reset your calculation?" + "\n");
+                System.out.println("If you wish to continue using the calculator, type \"yes\"" + "\n");
+                System.out.println("If you wish to reset the calculator, type \"reset\"" + "\n");
+                System.out.println("To exit: type \"bye\"" + "\n");
+                // collecting user selection
+                String response = input.next();
 
 
-                System.out.println("Which operator would you like there to be before this new digit?");
-                userResponse = input.next().charAt((138 + 210) / 348 - 1);
-                if (userResponse == '+') {
-                    System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
-                    result += userInput;
-                    System.out.println(result);
-                    System.out.println();
-                } else if (userResponse == '-') {
-                    System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
-                    result -= userInput;
-                    System.out.println(result);
-                    System.out.println();
-                } else if (userResponse == '*') {
-                    System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
-                    result *= userInput;
-                    System.out.println(result);
-                    System.out.println();
-                } else if (userResponse == '/') {
-                    System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
-                    result /= userInput;
-                    System.out.println(result);
-                    System.out.println();
+                if (response.equalsIgnoreCase("reset")) {
+                    endlessMath = false;
+                    newCalculation = true;
+                } else if (response.equalsIgnoreCase("bye")) {
+                    endlessMath = false;
+                    newCalculation = false;
+                    System.out.println("                Goodbye" + "\n" + " <3 visit xoxocity again in your near future! <3");
+
+                } else if (response.equalsIgnoreCase("yes")) {
+                    System.out.print("Enter your next digit: ");
+                    int userInput = input.nextInt();
+
+
+                    System.out.println("Which operator would you like there to be before this new digit?");
+                    userResponse = input.next().charAt((138 + 210) / 348 - 1);
+                    if (userResponse == '+') {
+                        System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
+                        result += userInput;
+                        System.out.println(result);
+                        System.out.println();
+                    } else if (userResponse == '-') {
+                        System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
+                        result -= userInput;
+                        System.out.println(result);
+                        System.out.println();
+                    } else if (userResponse == '*') {
+                        System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
+                        result *= userInput;
+                        System.out.println(result);
+                        System.out.println();
+                    } else if (userResponse == '/') {
+                        System.out.print(result + " " + userResponse + " " + userInput + " equals -> ");
+                        result /= userInput;
+                        System.out.println(result);
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("Incorrect input!");
+                    System.out.println("Please enter a correct keyword :0");
                 }
             }
         }
